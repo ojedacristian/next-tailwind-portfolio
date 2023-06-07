1fa70c9dc4f688456c1d0c8f5d75e500fbb4a1ff
Cristian Ojeda <cristianojeda.maimo@gmail.com>
2023-06-07 19:22:41 -0300

    simplificando el componente WorkCard

--- components/WorkCard.tsx
+++ components/WorkCard.tsx
@@ -8,34 +8,17 @@ interface WorkCardProps {
 
 export const WorkCard: React.FC<WorkCardProps> = ({align, work }) => {
     return (
-        <article className='flex flex-col md:flex-row my-14 lg:my-24 text-neutral-2'>
-            {
-                align % 2
-                    ?
-                    <>
-                        <div className='flex flex-col pb-8 text-right md:justify-end md:pr-8 md:pb-0 '>
+        <article className={`flex flex-col ${align % 2 ? 'md:flex-row-reverse' : 'md:flex-row'} my-14 lg:my-24 text-neutral-2`}>
+          
+                        <Image className='rounded-3xl' width={594} height={490} src={work.image} alt="" />
+                        <div className={`flex flex-col pb-8 ${align % 2 ? 'text-right md:pr-8' : 'text-left md:pl-8'} md:justify-end md:pb-0`}>
                             <h3 className='uppercase py-4 text-neutral-1 text-h5'>{ work.title }</h3>
                             <h3 className='text-h3 font-light leading-normal'>Lorem ipsum dolor sit amet, consectetur <b className='text-neutral-1'> adipiscing elit. Ultrices lorem non feugiat egestas amet.</b></h3>
                             <p className='my-6 font-light text-body'>{ work.category}</p>
-                            <button className='uppercase p-2 rounded-full w-40 border-2 text-neutral-1 border-neutral-2 text-h5 hover:bg-gradient-to-tl from-purple-600 to-pink-600 transition hover:scale-110 hover:translate-x-3'>
+                            <button className='uppercase p-2 rounded-full w-40 border text-neutral-1 border-neutral-2 text-h5 hover:bg-gradient-to-tl from-purple-600 to-pink-600 transition hover:scale-110 hover:translate-x-3 hover:border-white'>
                                 View Work
                             </button>
                         </div>
-                        <Image className='rounded-3xl' width={594} height={490} src={work.image} alt="" />
-                    </>
-                    :
-                    <>
-                        <Image className='rounded-3xl' width={594} height={490} src={work.image} alt="" />
-                        <div className='flex flex-col md:justify-end md:pl-8'>
-                            <h3 className='uppercase py-4 text-neutral-1 text-h5'>{ work.title }</h3>
-                            <h3 className='text-h3 font-light leading-normal'>Lorem ipsum dolor sit amet, consectetur <b className='text-neutral-1'> adipiscing elit. Ultrices lorem non feugiat egestas amet.</b></h3>
-                            <p className='my-6 font-light text-body'>{ work.category}</p>
-                            <button className='uppercase p-2 rounded-full w-40 border-2 text-neutral-1 border-neutral-2 text-h5'>
-                                View Work
-                            </button>
-                        </div>
-                    </>
-            }
         </article>
     )
 }
